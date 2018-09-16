78bf6b1b444a5eebaa4ab4f0f1eb9cd5df3d4a63
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-16 16:57:21 +0200

    Adding specs, json, wheeels

---
+++ spec/rps.spec.js
@@ -0,0 +1,9 @@
+const { rps } = require('./spec.helper')
+
+describe('rps', () => {
+    let rps = new Rps()
+
+    it ('expect true to be false',()=>{
+        expect(false).to.eql(true);
+    }) 
+})
